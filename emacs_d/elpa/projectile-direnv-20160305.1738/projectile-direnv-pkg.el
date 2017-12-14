aa9cf00f384d2e64da419a5e06fbff80afa82879
Mr Rogers <jon@carbonfive.com>
2017-12-13 20:16:29 -0800

    Merge with home

---
+++ emacs_d/elpa/projectile-direnv-20160305.1738/projectile-direnv-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "projectile-direnv" "20160305.1738" "Set environment variables from .envrc" '((emacs "24") (s "1.11.0") (dash "2.12.0") (projectile "0.13.0")) :commit "d5d29e5228f840b7a25358a2fd50353ef2dc9b16" :url "https://github.com/christianromney/projectile-direnv" :keywords '("convenience"))
--- emacs_d/elpa/projectile-git-autofetch-20161109.1429/projectile-git-autofetch-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "projectile-git-autofetch" "20161109.1429" "automatically fetch git repositories" '((projectile "0.14.0") (alert "1.2")) :url "https://github.com/andrmuel/projectile-git-autofetch" :keywords '("tools" "vc"))
